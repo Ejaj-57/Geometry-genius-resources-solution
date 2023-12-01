dbdce1b8976547cf4657346569f4635bd9c07a6c
K.M. Ejaj ul Ambia <k.m.ejajulambia@KMs-MacBook-Air.local>
2023-12-01 23:21:13 +0600

    common function for area calculation added

--- js/geometry.js
+++ js/geometry.js
@@ -1,22 +1,28 @@
 // common function
 function areaCalculation(parameter1, parameter2){
+// getting input1
+const input1Field = document.getElementById(parameter1);
+const input1String = input1Field.value;
+const input1Value = parseInt(input1String);
+// getting input2
+const input2Field = document.getElementById(parameter2);
+const input2String = input2Field.value;
+const input2Value = parseInt(input2String);
 
+// multiply
+const commonArea = input1Value * input2Value;
+return commonArea;
 }
 
 
 
 document.getElementById('btn-triangle-area').addEventListener
 ('click', function(){
-    // getting input1
-    const input1Field = document.getElementById('triangle-b');
-    const input1String = input1Field.value;
-    const input1Value = parseInt(input1String);
-    // getting input2
-    const input2Field = document.getElementById('triangle-h');
-    const input2String = input2Field.value;
-    const input2Value = parseInt(input2String);
+    const commonAreaTriangle = areaCalculation('triangle-b', 'triangle-h');
+    console.log(commonAreaTriangle);
+
     // calculating area
-    const areaTriangle = 0.5 * input1Value * input2Value;
+    const areaTriangle = 0.5 * commonAreaTriangle;
 
     // setting area value into area calculation card
     const triangleAreaValueField = document.getElementById('triangle-area-value');
